40af42908ce8c7b24796e607162ce999ca828ec7
Kristian Mandrup <kmandrup@gmail.com>
2010-08-24 18:03:51 +0200

    improved multi-alias

--- lib/sugar-high/alias.rb
+++ lib/sugar-high/alias.rb
@@ -9,16 +9,35 @@ class Module
   # insert_into_xxx becomes inject_into_xxx and update_xxx
   # read_xxx becomes xxx_content (overriding default :after action to insert at the X)
   
-  def multi_alias name, options={}
-    config_options = options[:options]
+  def multi_alias *args
+    name = case args.first
+    when Symbol, String            
+      args.first.to_s
+    when Hash    
+      # default is :after
+      args.first[:_before_] ? :before : :after
+    end
+    
+    if name.kind_of? Symbol
+      config_options = name
+      options = args.first
+      name = options[:"_#{name}_"]
+    else
+      options = args[1]
+    end          
+    
+    raise ArgumentError, "Name of method pattern to alias not specified. Please pass name as either first argument or as :before or :after option" if !name
+
+    options.delete(:_after_)
+    options.delete(:_before_)
+    
     options.each_pair do |original, aliases|
-      next if original == :options 
-      alias_methods name, original, [aliases].flatten, config_options
+      alias_methods name.to_sym, original, [aliases].flatten, config_options
     end            
   end
 
   def alias_methods name, original, aliases, config_options
-    aliases.each do |alias_name| 
+    aliases.each do |alias_name|                       
       new_alias     = make_name(name, alias_name.to_s, config_options)
       original_name = make_name(name, original.to_s, config_options)
       begin 
@@ -32,12 +51,12 @@ class Module
   protected
 
   def make_name name, alias_name, config_options
-    return alias_name.gsub(/X/, name.to_s) if alias_name =~ /X/
+    return alias_name.gsub(/X/, name.to_s) if alias_name =~ /X/    
     case config_options
-    when :after
-      "#{alias_name}_#{name}"
     when :before
       "#{name}_#{alias_name}"
+    else # default
+      "#{alias_name}_#{name}"
     end
   end
 end
--- spec/sugar-high/alias_spec.rb
+++ spec/sugar-high/alias_spec.rb
@@ -10,24 +10,40 @@ require 'sugar-high/alias'
 
 class Abc
   def hello_kristian
+    'hello'
+  end
+  multi_alias :_after_ => :kristian, :hello => :howdy
+end
+
+class Xyz  
+  def hello_kristian
     'hi'
   end
-  multi_alias :kristian, :hello => :howdy, :options => :after
+
+  multi_alias :kristian, :hello => :alloha
+end
+
+class Ged  
+  def kristian_hello
+    'hejsa'
+  end
+  
+  multi_alias :_before_ => :kristian, :hello => :hejsa  
 end  
 
 describe "SugarHigh" do
-  describe "Arguments" do
-    before do
-      @obj = Abc.new
-    end
-    
-    context 'Aliased :hello_kristian with :howdy_kristian ' do
+  describe "Arguments" do    
+    context 'hould alias :hello_kristian with :howdy_kristian ' do
       it "should find alias" do
-        @obj.respond_to?(:howdy_kristian).should be_true
+        Abc.new.respond_to?(:howdy_kristian).should be_true
       end
 
-      it "should find all methods saying 'hi' to kristian" do
-        Abc.new.get_methods(:all).sort.grep(/(.*)_kristian$/).should have(2).items
+      it "should find -alloha alias method for kristian" do
+        Xyz.new.respond_to?(:alloha_kristian).should be_true
+      end
+
+      it "should find -hejsa alias method for kristian" do
+        Ged.new.respond_to?(:kristian_hejsa).should be_true
       end
     end
   end
