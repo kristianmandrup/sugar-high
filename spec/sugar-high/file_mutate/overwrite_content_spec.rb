e9311be51fa63914f7819ab21ab360f7555fe7fc
Kristian Mandrup <kmandrup@gmail.com>
2011-05-18 11:48:25 +0200

    overwrite

--- spec/sugar-high/file_mutate/overwrite_content_spec.rb
+++ spec/sugar-high/file_mutate/overwrite_content_spec.rb
@@ -3,65 +3,64 @@ require 'sugar-high/file_mutate'
 
 describe "Overwrite file" do
   let(:empty_file)      { fixture_file 'empty.txt' }
+  let(:non_empty_file)  { fixture_file 'non-empty.txt' }
   let(:content_file)    { fixture_file 'content_file.txt' }
 
-  describe '#overwrite! (Class method)' do
-    let (:content) { 'Overwritten!!!' }
+  describe 'File #overwrite class method'
+    describe '#overwrite with block arg' do
+      context 'empty file' do 
+        let (:content) { 'Overwritten!!!' }
   
-    it 'should overwrite empty file' do      
-      File.overwrite content_file do
-        content
+        it 'should overwrite empty file' do      
+          File.overwrite content_file do
+            content
+          end
+          File.has_content?(content).should be_true
+          File.delete! content_file
+        end
       end
-      File.has_content?(content).should be_true
-      File.delete! content_file
-    end
-
-    it 'should overwrite file content with new content, erasing the old' do
-      File.overwrite content_file do
-        content
-      end
-
-      new_content = "New content"
-      File.overwrite content_file do
-        new_content        
-      end
-
-      content_file.has_content?(content).should be_false
-      content_file.has_content?(new_content).should be_true      
-    end
-  end
     
-  describe '#overwrite! (instance)' do
-    let (:content) { 'Overwritten!!!' }
-  
-    it 'should overwrite empty file' do      
-      content_file.overwrite do
-        content
+      context 'non-empty file' do     
+        it 'should overwrite file content with new content, erasing the old' do
+          File.overwrite content_file do
+            content
+          end
+
+          new_content = "New content"
+          File.overwrite content_file do
+            new_content        
+          end
+
+          content_file.has_content?(content).should be_false
+          content_file.has_content?(new_content).should be_true      
+        end
       end
-      content_file.has_content?(content).should be_true
-      File.delete! content_file
     end
-
-    it 'should overwrite file content with new content, erasing the old' do
-      content_file.overwrite :with => content
-
-      new_content = "New content"
-      content_file.overwrite :with => new_content
-
-      content_file.has_content?(content).should be_false
-      content_file.has_content?(new_content).should be_true      
-    end
-  end
+    
+    describe '#overwrite! (instance)' do
+      let (:content) { 'Overwritten!!!' }
   
-  describe '#overwrite with :content option' do      
-    it 'should overwrite content' do      
-      File.overwrite content_file, :with => content
+      describe '#overwrite with block argument'
+        it 'should overwrite empty file' do      
+          content_file.overwrite do
+            content
+          end
+          content_file.has_content?(content).should be_true
+          File.delete! content_file
+        end
+      end
 
-      new_content = "New content"
-      File.overwrite content_file, :with => new_content
+      describe '#overwrite using :with option arg'
+        it 'should overwrite file content with new content, erasing the old' do
+          content_file.overwrite :with => content
 
-      content_file.has_content?(content).should be_false
-      content_file.has_content?(new_content).should be_true      
+          new_content = "New content"
+          content_file.overwrite :with => new_content
+
+          content_file.has_content?(content).should be_false
+          content_file.has_content?(new_content).should be_true
+        end
+      end
     end
-  end 
+  end  
 end
