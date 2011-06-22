5cf665b8ee99df8c286a2c01256669421021d01c
Kristian Mandrup <kmandrup@gmail.com>
2011-06-22 10:12:44 +0200

    improved numeric pack

--- lib/sugar-high/numeric.rb
+++ lib/sugar-high/numeric.rb
@@ -1,11 +1,20 @@
 module NumericCheckExt
   def is_numeric? arg
     arg.is_a? Numeric
-  end  
-
-  alias_method :is_num?, :is_numeric?
+  end
+  alias_method :numeric?, :is_numeric?
 
   def check_numeric! arg
     raise ArgumentError, "Argument must be Numeric" if !is_numeric? arg
+  end  
+end
+
+module NumberDslExt   
+  def thousand
+    self * 1000
+  end  
+
+  def hundred
+    self * 100
   end  
 end
---
+++ spec/sugar-high/numeric_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+require 'sugar-high/numeric'
+
+class Numeric
+  include NumberDslExt  
+end    
+
+module Num
+  extend NumericCheckExt
+end
+
+describe "SugarHigh" do
+  describe 'NumericCheckExt' do
+    describe '#numeric?' do
+      it 'string "x1" is not numeric' do
+        Num.numeric?("x0").should be_false
+      end
+
+      it '123 is numeric' do
+        Num.numeric?(123).should be_true
+      end
+
+      it '12.3 is numeric' do
+        Num.numeric?(12.3).should be_true
+      end
+    end
+    
+    describe 'check_numeric!' do
+      it 'string "x1" is not numeric' do
+        lambda {Num.check_numeric!("x0")}.should raise_error
+      end
+
+      it '123 is numeric' do
+        lambda {Num.check_numeric!(123)}.should_not raise_error
+      end
+    end
+  end
+  
+  describe 'NumberDslExt' do
+    describe '#hundred' do
+      it '2 hundred is 200' do
+        2.hundred.should == 200
+      end
+    end
+    describe '#thousand' do
+      it '3 thousand is 3000' do
+        3.thousand.should == 3000
+      end
+    end
+  end
+end
+      
