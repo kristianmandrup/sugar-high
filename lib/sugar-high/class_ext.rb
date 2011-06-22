75042367ce484dadfaf48dfabe4555d7cc7ab9d8
stanislaw <s.pankevich@gmail.com>
2011-06-22 21:34:35 +0300

    very small fix of include_and_extend

--- lib/sugar-high/class_ext.rb
+++ lib/sugar-high/class_ext.rb
@@ -5,7 +5,7 @@ class Class
     options[:instance_methods] ||= :InstanceMethods
     options[:class_methods] ||= :ClassMethods
     # Mainly include but be flexible
-    main_module = const_get(the_module)
+    main_module = const_get(the_module.to_s.to_sym)
     include main_module # for an extend_and_include method, change this to extend main_module
     include main_module.const_get(options[:instance_methods]) if main_module.const_defined?(options[:instance_methods])
     extend main_module.const_get(options[:class_methods]) if main_module.const_defined?(options[:class_methods])
