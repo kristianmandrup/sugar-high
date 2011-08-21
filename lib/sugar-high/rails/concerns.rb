1888649d65370c2946ba21443ebd90d5dbbd861d
stanislaw <s.pankevich@gmail.com>
2011-08-21 16:57:05 +0300

    enabled #concerned_with method + added specs

--- lib/sugar-high/rails/concerns.rb
+++ lib/sugar-high/rails/concerns.rb
@@ -1,23 +1,29 @@
-class << Module
+class Module
   def concerned_with(*concerns)
     concerns.each do |concern|
-      require_dependency "#{name.underscore}/#{concern}"
+      require_method "#{name.underscore}/#{concern}"
     end
   end
 
   def shared_concerns(*concerns)
     concerns.each do |concern|
-      require_dependency "shared/#{concern}"
+      require_method "shared/#{concern}"
     end
   end
 
   def include_shared_concerns(*concerns)
     concerns.each do |concern|
-      require_dependency "shared/#{concern}"
+      require_method "shared/#{concern}"
       self.send :include, concern.to_s.camelize.constantize
     end
   end
 
   alias_method :shared_concern, :shared_concerns
   alias_method :include_shared_concern, :include_shared_concerns
+
+  protected
+
+  def require_method path
+    defined?(require_dependency) ? require_dependency(path) : require(path)
+  end
 end
---
+++ spec/sugar-high/rails/fixture_user/scopes.rb
@@ -0,0 +1,4 @@
+class FixtureUser
+  def method_from_scopes_concern
+  end
+end
