588f49b00456a8368ec95b52e42d12a02170355d
Kristian Mandrup <kmandrup@gmail.com>
2012-09-25 09:30:35 +0200

    added delegate decorator

--- lib/sugar-high/delegate.rb
+++ lib/sugar-high/delegate.rb
@@ -38,3 +38,17 @@ class Module
     end
   end
 end
+
+# http://blog.jayfields.com/2008/02/ruby-replace-methodmissing-with-dynamic.html
+class DelegateDecorator
+  def initialize(subject)
+    subject.public_methods(false).each do |meth|
+      (class << self; self; end).class_eval do
+        define_method meth do |*args|
+          subject.send meth, *args
+        end
+      end
+    end
+  end
+end
+
