65151c2413f5cf12c6f49042cfe1312208074f70
Kristian Mandrup <kmandrup@gmail.com>
2012-09-25 09:55:15 +0200

    range works

--- lib/sugar-high/range.rb
+++ lib/sugar-high/range.rb
@@ -1,4 +1,16 @@
 class Range  
+  def intersection(other)  
+    raise ArgumentError, 'value must be a Range' unless other.kind_of?(Range)  
+  
+    min, max = first, exclude_end? ? max : last  
+    other_min, other_max = other.first, other.exclude_end? ? other.max : other.last  
+  
+    new_min = self === other_min ? other_min : other === min ? min : nil  
+    new_max = self === other_max ? other_max : other === max ? max : nil  
+  
+    new_min && new_max ? new_min..new_max : nil  
+  end
+
   def intersect(other)  
     raise ArgumentError, 'value must be a Range' unless other.kind_of?(Range)  
 
@@ -6,7 +18,9 @@ class Range
     new_max = self.cover?(other.max) ? other.max : other.cover?(max) ? max : nil  
 
     new_min && new_max ? new_min..new_max : nil  
+
+  rescue # if above doesn't work for ruby version
+    intersection(other)
   end  
-  alias_method :intersection, :intersect
-  alias_method :&, :intersection  
+  alias_method :&, :intersect
 end
